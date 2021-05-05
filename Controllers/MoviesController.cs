a10201dbd3470cb55bbc79f3ace9afc2cfe593bc
razvanlutac <r.lutac01@gmail.comgit config --global user.email r.lutac01@gmail.com>
2021-05-05 18:25:48 +0300

    Add project files.

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private static List<Models.Movie> movies = new List<Models.Movie>()
+        { 
+             new Models.Movie() { ID = Guid.NewGuid(), MovieName = "Movie1" },
+            new Models.Movie() { ID = Guid.NewGuid(), MovieName = "Movie2" },
+            new Models.Movie() { ID = Guid.NewGuid(), MovieName = "Movie3" },
+            new Models.Movie() { ID = Guid.NewGuid(), MovieName = "Movie4" },
+            new Models.Movie() { ID = Guid.NewGuid(), MovieName = "Movie5" }
+        };
+
+[HttpGet]
+public Models.Movie[] Get()
+{
+    return movies.ToArray();
+}
+
+[HttpPost]
+public void Post([FromBody] Models.Movie movie)
+{
+    if (movie.ID == Guid.Empty)
+        movie.ID = Guid.NewGuid();
+
+    movies.Add(movie);
+}
+
+[HttpPut]
+public void Put([FromBody] Models.Movie movie)
+{
+    Models.Movie currentMovie = movies.FirstOrDefault(x => x.ID == movie.ID);
+    currentMovie.MovieName = movie.MovieName;
+}
+
+[HttpDelete]
+public void Delete(Guid id)
+{
+    movies.RemoveAll(movie => movie.ID == id);
+
+    //foreach (Models.Movie movie in movies)
+    //{
+    //    if (movie.ID == id)
+    //    {
+    //        movies.Remove(movie);
+    //    }
+    //}
+}
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Movie
+    {
+
+        public Guid ID { get; set; }
+
+        public string MovieName { get; set; }
+
+        public string Genre { get; set; }
+    }
+}
